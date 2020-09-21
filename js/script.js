e56ff09715ec1e6c90ffd4da4899b0a7061a6871
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-21 11:24:09 +0200

    aggiustamento codice

--- js/script.js
+++ js/script.js
@@ -1,42 +1,61 @@
 $(document).ready(function(){
- // data iniozio calendario
-  var date = "2018-01-01";
+ // data inizio calendario
+  var date = moment("2018-01-01");
 
-  var momentDate = moment(date);
+  printCalendar(date);
+  printHolidays(date);
 
-// TEMPLATE DEL CALENDARIO dei giorni
-  var source = $("#day-template").html();
-  var template = handlebars.compile(source);
+});
 
-// stamppo i giorni del mio calendario
-  var dateCompleteMoment = moment(date);
+function printCalendar(date){
+    var momentDate = moment(date);
 
-  // STAMPO GIORNI del calendario
-  for (var i = 0; i <= momentDate.daysInMoment(); i++) {
-    var context = {
-      "day": i,
-      "month": momentDate.format("MMMM"),
-      "dateComplete": momentDate.format("YYYY-MM-DD")
-    };
+    $("h1").text(momentDate.format("MMMM YYYYY"));
+    // TEMPLATE DEL CALENDARIO dei giorni
+    var source = $("#day-template").html();
+    var template = Handlebars.compile(source);
 
-    var html = template(context);
+        // STAMPO GIORNI del calendario
+    for (var i = 0; i <= momentDate.daysInMoment(); i++) {
+      var dateComplate = momentDate.format("YYYY")+"-"+ momentDate.format("MM")+"-"+addZero(i);
+      var context = {
+        "day": i,
+        "month": momentDate.format("MMMM"),
+        "dateComplete": dateComplate
+      };
 
-    $("#days").append(html);
-    console.log(momnetDate);
-    dateComplateMoment.add(1, "date");
+      var html = template(context);
 
-  }
+      $("#days").append(html);
+
+    }
+}
+
+
+function printHolidays(holidays){
+  var month = momentDate.format("M")-1;
 
   $.ajax(
     {
-      "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+      "url": "https://flynn.boolean.careers/exercises/api/holidays",
       "data": {
         "year": 2018,
-        "month": 0
+        "month": month
       },
       "method": "GET",
       "success": function(data){
-        printHolidays(data.response)
+        var holidays = data.response;
+
+        if(holidays.lenght > 0){
+          for (var i = 0; i < holidays.length; i++) {
+
+            var holidaysDate = holidays[i].date;
+            var holidaysName = holidays[i].name;
+
+            $(".day[data-date = '"+holidayDate+"']").addClass("holiday");
+            $(".day[data-date = '"+holidayDate+"'].holidayType").text("- "+ holidayName);
+          }
+        }
       },
       "error": function(prova){
         alert("Errore!");
@@ -44,15 +63,11 @@ $(document).ready(function(){
     }
   );
 
-  function printHolidays(holidays){
-    if(holidays.lenght > 0){
-      for (var i = 0; i < holidays.length; i++) {
-        var holidaysDate = holidays[i].date;
-        var holidaysName = holidays[i].name;
-        $(".dav[data-date = '"+holidavDate+"']").addClass("holidav";)
-    }
+}
+
+
+function addZero(num){
+  if(num < 10){
+    return "0"+num;
   }
-
-
-
-});
+}
