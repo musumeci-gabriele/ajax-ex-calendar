29fae8f9d25196e73ee4318d5c42fa987182c25e
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-21 17:39:27 +0200

    errore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
+// FUNZIONE STAMPA CALENDARIO
+function printCalendar(date){
+  var dateCalendar = moment(date);
+  // Pulizia della lista
+  $("#days").html("");
+
+  // Intestazione del mese
+  $("h1").text(dateCalendar.format("MMMM YYYY"));
+
+  var dayInMonth = dateCalendar.daysInMonth();
+
+  // TEMPLATE DEL CALENDARIO dei giorni
+  var source = $("#day-template").html();
+  var template = Handlebars.compile(source);
+
+  // STAMPA GIORNI DEL CALENDARIO
+  for (var i = 0; i <= dayInMonth; i++) {
+    var context = {
+      "day": i,
+      "month": dateCalendar.format("MMMM"),
+      "dateComplete": dateCalendar.format("YYYY-MM-DD")
+    };
+
+    var html = template(context);
+
+    $("#days").append(html);
+    dateCalendar.add(1, "days");
+
+  }
+}
+
+// FUNZIONE STAMPA FESTIVITA' CON CHIAMATA ALL'API E STAMPA LE FESTIVITA'
+function printHolidays(date){
+  // chiamata al server del calendario 2018
+  $.ajax(
+    {
+      "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+      "data": {
+        "year": 2018,
+        "month": date.format("M")-1
+      },
+      "method": "GET",
+      "success": function(data){
+        var holidays = data.response;
+        for (var i = 0; i < holidays.length; i++) {
+          var holidaysDate = holidays[i].date;
+          var holidaysName = holidays[i].name;
+
+          $(".day[data-date = '"+ holidaysDate +"']").addClass("redHoliday");
+          $(".day[data-date = '"+ holidaysDate +"'] span").text("- "+ holidaysName);
+          }
+      },
+      "error": function(){
+        alert("Errore!");
+      }
+    }
+  );
+}
+
 $(document).ready(function(){
  // DATA INIZIO CALENDARIO
   var date = moment("2018-01-01");
@@ -7,71 +66,12 @@ $(document).ready(function(){
 
 });
 
-// FUNZIONE STAMPA CALENDARIO
-function printCalendar(date){
-    var momentDate = moment(date);
-
-    $("h1").text(momentDate.format("MMMM YYYY"));
-    // TEMPLATE DEL CALENDARIO dei giorni
-    var source = $("#day-template").html();
-    var template = Handlebars.compile(source);
-
-    // STAMPA GIORNI DEL CALENDARIO
-    for (var i = 0; i <= momentDate.daysInMoment(); i++) {
-
-      var dateComplate = momentDate.format("YYYY")+"-"+ momentDate.format("MM")+"-"+addZero(i);
-
-      var context = {
-        "day": i,
-        "month": momentDate.format("MMMM"),
-        "dateComplete": dateComplate
-      };
-
-      var html = template(context);
-
-      $("#days").append(html);
-
-    }
-}
-
-// FUNZIONE STAMPA FESTIVITA'
-function printHolidays(holidays){
-  // var mese
-  var month = date.format("M")-1;
-  // chiamata al server del calendario 2018
-  $.ajax(
-    {
-      "url": "https://flynn.boolean.careers/exercises/api/holidays",
-      "data": {
-        "year": 2018,
-        "month": month
-      },
-      "method": "GET",
-      "success": function(data){
-        var holidays = data.response;
-
-        if(holidays.lenght > 0){
-          for (var i = 0; i < holidays.length; i++) {
-
-            var holidaysDate = holidays[i].date;
-            var holidaysName = holidays[i].name;
-
-            $(".day[data-date = '"+ holidaysDate +"']").addClass("RedHoliday");
-            $(".day[data-date = '"+ holidaysName +"'].holiday").text("- "+ holidaysName);
-          }
-        }
-      },
-      "error": function(){
-        alert("Errore!");
-      }
-    }
-  );
-
-}
-
-
-function addZero(num){
-  if(num < 10){
-    return "0"+num;
+$(".prev").click(function(){
+  if(date.format("M") == 1){
+    alert("non puoi andare indietro");
+  }else{
+    date.subtract(1, "months");
+    printCalendar(date);
+    printHolidays(date);
   }
-}
+});
